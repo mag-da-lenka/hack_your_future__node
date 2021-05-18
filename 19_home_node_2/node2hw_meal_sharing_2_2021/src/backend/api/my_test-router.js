5b12ea1953f9d22af203bf2daba6da3893559e40
mag-da-lenka <magdalenka.ag@gmail.com>
2021-05-18 20:21:46 +0200

    test updated

--- 19_home_node_2/node2hw_meal_sharing_2_2021/src/backend/api/my_test-router.js
+++ 19_home_node_2/node2hw_meal_sharing_2_2021/src/backend/api/my_test-router.js
@@ -23,18 +23,19 @@ const reviews_INITIAL =
 
 router.get('/', async (reqest, response) => {
 
-    const jsons = {
-        "a) this is": "------- my_test for jsons I'm using for this app --------",
-        json_1_MEALS: meals,
-        json_2_REVIEWS: reviews,
-        json_3_RESERVATIONS: reservations,
-        "b) this is": "------- my_test for INITIAL jsons before they were transformed --------",
-        json_1_MEALS_INITIAL: meals_INITIAL,
-        json_2_REVIEWS_INITIAL: reviews_INITIAL,
-        json_3_RESERVATIONS_INITIAL: reservations_INITIAL
-    }
-
     try {
+
+        const jsons = {
+            "a) this is": "------- my_test for jsons I'm using for this app --------",
+            json_1_MEALS: meals,
+            json_2_REVIEWS: reviews,
+            json_3_RESERVATIONS: reservations,
+            "b) this is": "------- my_test for INITIAL jsons before they were transformed --------",
+            json_1_MEALS_INITIAL: meals_INITIAL,
+            json_2_REVIEWS_INITIAL: reviews_INITIAL,
+            json_3_RESERVATIONS_INITIAL: reservations_INITIAL
+        }
+
         response.json(jsons)
         // link to check:
         // http://localhost:3000/api/my_test     
